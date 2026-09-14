d318bd090b113d34acd4f8e114533b6e7445e7ca
c00324897 <c00324897@setu.ie>
2026-09-14 11:05:33 +0000

    Complete lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("what is your name")
+id_number = input("ehat is your student number")
+year = input("what is your year of study")
+
+print("Name = " + name)
+print("student number = " + id_number)
+prin("Year = " + year)
